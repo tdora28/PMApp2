14bf95aa93b4256987dede05eab26df09baf3f8a
Santosh Kalwar <santoshkalwar@gmail.com>
2023-01-25 12:10:17 +0200

    Initial commit

---
+++ dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log('PMApp in TS begins...');
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+const prjInput = new ProjectInput();
