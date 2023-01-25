6866a4500108dbc5aada9eea11cbad21abde0a32
Santosh Kalwar <santoshkalwar@gmail.com>
2023-01-25 13:20:42 +0200

    Update with autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
-console.log('PMApp in TS begins...');
+// decorator is a function
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
 
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -34,13 +45,14 @@ class ProjectInput {
     this.configure();
   }
 
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
   }
 
   private configure() {
-    this.element.addEventListener('submit', this.submitHandler.bind(this));
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
