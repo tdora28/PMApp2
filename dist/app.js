cd73747b78ed600bb770c28339877cb14f5a2f74
Dóra Tokai <tokaidora88@gmail.com>
2024-01-30 13:40:49 +0200

    Init TypeScript

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+import { ProjectInput } from './components/project-input';
+import { ProjectList } from './components/project-list';
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
